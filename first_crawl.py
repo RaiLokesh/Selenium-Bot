56a0186d766ea3bf132ffa996102fe482e9acf59
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-02-10 18:50:48 +0530

    add mailer

--- first_crawl.py
+++ first_crawl.py
@@ -10,7 +10,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=6)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
 
 def scheduled_job():
 
@@ -21,7 +21,7 @@ def scheduled_job():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options) #'C:\chromedriver\chromedriver.exe'
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
 
     url = 'https://lmsone.iiitkottayam.ac.in/login/'
     browser.get(url)
@@ -33,9 +33,9 @@ def scheduled_job():
     search.send_keys(m_pass)
 
     submit = browser.find_element_by_id('loginbtn')
-    #sleep(2)
+    sleep(2)
     submit.click()
-    #sleep(5)
+    sleep(5)
 
     url = 'https://lmsone.iiitkottayam.ac.in/calendar/view.php'
     browser.get(url)
@@ -78,37 +78,40 @@ def scheduled_job():
 
     if mail=='': mail="abhi checking chalu hai"
     d = {"task": mail}
+    '''
     if mail:
         r = requests.post('https://hook.integromat.com/pxfywsoha4hey2h4hgyakhgicn7erj3h', data=json.dumps(d), headers={'Content-Type':'application/json'})
+    '''
+
+#------Mailing Scenes start here!!--------
+    
+    if mail:
+        print("pikachu")
+        recievers = []
+        sheet_json = os.environ.get("JSON")
+        spreadsheet_data = requests.get(sheet_json)
+        spreadsheet_data = spreadsheet_data.json()
+
+        for i in spreadsheet_data:
+            if i['Select Yes/No.'] == 'Yes':
+                recievers.append(i['Email Address'])
+
+        for i in recievers:
+            msg = MIMEMultipart()
+            maill = os.environ.get("EMAIL")
+            passs = os.environ.get("PASSWORD")
+            msg['From'] = maill
+            msg['To'] = i
+            msg['Subject'] ="Assignemnts To Be Submitted Today!"
+            body = 'Hello,\nThe assignments that require your attention today are listed below\n\n'+mail+'\n\nRegards.'
+            msg.attach(MIMEText(body, 'plain'))
+            text = msg.as_string()
+            smtp_server = os.environ.get("SMTP_SERVER")
+            sm= smtplib.SMTP(smtp_server, 587)
+            sm.starttls()
+            sm.login(maill, passs)
+            sm.sendmail(maill, i, text)
+            sm.quit()
+    
 
 sched.start()
-#------Mailing Scenes start here!!--------
-'''
-if mail:
-    print("pikachu")
-    recievers = []
-    sheet_json = os.environ.get("JSON")
-    spreadsheet_data = requests.get(sheet_json)
-    spreadsheet_data = spreadsheet_data.json()
-
-    for i in spreadsheet_data:
-        if i['Select Yes/No.'] == 'Yes':
-            recievers.append(i['Email Address'])
-
-    for i in recievers:
-        msg = MIMEMultipart()
-        maill = os.environ.get("EMAIL")
-        passs = os.environ.get("PASSWORD")
-        msg['From'] = maill
-        msg['To'] = i
-        msg['Subject'] ="Assignemnts To Be Submitted Today!"
-        body = 'Hello,\nThe assignments that require your attention today are listed below\n\n'+mail+'\n\nRegards.'
-        msg.attach(MIMEText(body, 'plain'))
-        text = msg.as_string()
-        #smtp_server = os.environ.get("SMTP_SERVER")
-        sm= smtplib.SMTP("smtp.gmail.com", 587)
-        sm.starttls()
-        sm.login(maill, passs)
-        sm.sendmail(maill, i, text)
-        sm.quit()
-'''
