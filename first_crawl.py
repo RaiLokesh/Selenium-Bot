3a513f7d51ac725e1d598c526bac1386f436f524
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-02-11 08:12:15 +0530

    changes

--- first_crawl.py
+++ first_crawl.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=42)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=45)
 
 def scheduled_job():
 
